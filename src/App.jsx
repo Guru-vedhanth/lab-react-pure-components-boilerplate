97fc505270e4be5a1264d7b1ee82af546b81670b
GURUVEDHANTH S <officialvedhanth@gmail.com>
2023-12-11 16:20:42 +0530

    Completed..

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Pure from './components/Pure'
+import Regular from './components/Regular'
+
+function App() {
+
+  return (
+    <>
+      <Regular />
+      <Pure />
+    </>
+  )
+}
+
+export default App
